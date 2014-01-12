4a981e5a039fd9cb692665397887f39386bcf0da
Vincent.Tone <tongwensong@gmail.com>
2014-01-12 15:27:13 +0800

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def au_entry():
     return 'hello world!'
+
+@app.route('/article/<path:action>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def au_article(action):
+    # get article config
+    # get post put delete article
+    return action
 
 if __name__ == '__main__':
     app.run()
