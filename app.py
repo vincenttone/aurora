8b8d63138b836d5f2d865460d17f05149fab329f
Vincent.Tone <tongwensong@gmail.com>
2014-01-12 15:36:18 +0800

    utf8

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+#-*- coding:utf-8 -*-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def au_entry():
-    return 'hello world!'
+    return '项目首页'
 
 @app.route('/article/<path:action>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def au_article(action):
